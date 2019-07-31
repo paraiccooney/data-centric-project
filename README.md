f7f4a2562d0217c02456168538513e4d06229055
Ubuntu <ubuntu@ip-172-31-94-210.ec2.internal>
2019-07-31 12:41:07 +0000

    modal added & landing routes amended

---
+++ README.md
@@ -0,0 +1,1 @@
+Startup modal credits - https://www.tutorialrepublic.com/faq/how-to-launch-bootstrap-modal-on-page-load.php
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_pymongo import PyMongo
 # Mongo stores it's data in BSON format which is similar to JSON. We use this is the '/edit_task route/<task_id>' route below
 # this allows us to convert our task id into bson format
@@ -16,11 +16,46 @@ app.config["MONGO_URI"] = os.getenv("MONGOURI")
 
 # instance of PyMongo created
 mongo = PyMongo(app)
-
+app.secret_key = "randomstring123"
+    
+"""    
 # / refers to the default route.
 @app.route('/')
-def my_recipes():
-    return render_template("index.html",recipies=mongo.db.recipes.find())
+def welcome_page():
+    return render_template("index.html")
+
+
+#route for my recipes
+@app.route('/my_recipes', methods=["GET", "POST"])
+def my_recipes(username):
+    username=username;
+    return render_template("myrecipes.html",recipes=mongo.db.recipes.find(), username=username)"""
+    
+####################################
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """Main page with instructions"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+        
+    # if username has been submitted then return the below page (url_for redirects to the function name as opposed to the @app.route)
+    if "username" in session:
+        return redirect(url_for("my_recipes", username=session["username"]))
+        
+    # if username has not been submitted index.html will be rendered
+    return render_template("index.html")
+
+
+@app.route("/recipes/<username>", methods=["GET", "POST"])
+def my_recipes(username):
+    if request.method == "POST":
+        username = session["username"]
+        return redirect(url_for("my_recipes", username=session["username"]))
+
+    return render_template("myrecipes.html", username=username)
+####################################
+
+
 
 # route for upload recipe
 @app.route('/upload_recipe')
@@ -29,9 +64,9 @@ def upload_recipe():
                            categories=mongo.db.recipe_categories.find())
 
 # route for random recipe
-@app.route('/saved_recipies')
-def saved_recipies():
-    return render_template("saved_recipies.html")
+@app.route('/saved_recipes')
+def saved_recipes():
+    return render_template("saved_recipes.html")
     
 # route for search
 @app.route('/search_recipes')
@@ -40,10 +75,12 @@ def search_recipes():
     
 @app.route('/upload_recipe_button', methods=['POST'])
 def upload_recipe_button():
-    recipies = mongo.db.recipes
-    recipies.insert_one(request.form.to_dict())
-    # redirect to my recipies page
-    return render_template("index.html",recipies=mongo.db.recipes.find())
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    # redirect to my recipes page
+    return render_template("index.html",recipes=mongo.db.recipes.find())
+
+
 
 # runs the app (instance created above)
 if __name__ == '__main__':
